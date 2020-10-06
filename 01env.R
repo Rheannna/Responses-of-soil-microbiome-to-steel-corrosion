0afdec76941fb8fd73ec64f30c4525ab5f7f87dc
Rheannna <38430563+Rheannna@users.noreply.github.com>
2020-10-06 15:53:42 +0800

    Update 01env.R

--- 01env.R
+++ 01env.R
@@ -9,14 +9,14 @@ by(env$Cd.mg.kg., env$date, mean)
 aggregate(C ~ date,env[3:36,],mean)
 aggregate(C ~ date,env[3:36,],sd)
 
-##多组分组
+##
 sd = aggregate(env[,c(2:16)], by = list(env$date), sd)
 mean = aggregate(env[,c(2:16)], by = list(env$date), mean)
 
 
 rnorm(40, mean=1.82, sd =0.2390)
 
-# 统计数据
+# 
 library(pastecs)
 s3 <- stat.desc(env)
 
